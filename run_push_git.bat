fdd7bec8b54beb9383806b00423b76f9ea7c330d
colaCheng <colachengcheng@gmail.com>
2023-04-04 17:05:15 +0800

    upload

---
+++ run_push_git.bat
@@ -0,0 +1,5 @@
+git add readme.md readme.assets run_push_git.bat map_creator.py 
+git commit -m "upload"
+git branch -M main
+git remote add origin https://github.com/shaochengyan/ColaUsefullTools.git
+git push -u origin main
